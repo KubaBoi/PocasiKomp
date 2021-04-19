4a1c48f7fbbf4fe73a076fdd909e774e09a1169d
KubaBoi <jakubanderle@outlook.cz>
2021-04-20 01:56:31 +0200

    add getDevices

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 import tkinter as tk
 import requests
+from subprocess import call, check_output
 
 print("Starting program")
 
@@ -97,6 +98,18 @@ class SampleApp(tk.Tk):
 
         r = requests.get(url = URL, params = PARAMS)
         data = r.json()
+
+    def getDevices(self):
+    output = check_output(['hcitool', 'con']).decode("utf-8") 
+    lines = output.split(">")
+
+    devices = []
+    for d in range(1, len(lines)):
+        mac = lines[d].strip().split(" ")[1] #ziska mac adresu
+        name = check_output(["hcitool", "name", mac]).decode("utf-8").rstrip() #ziska jmeno zarizeni
+        devices.append((name, mac))
+
+    return devices
 
 if __name__== "__main__":
     try:
