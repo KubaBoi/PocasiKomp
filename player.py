1a49bc9703cb8f2be4affc90da217e870ef5b198
KubaBoi <jakubanderle@outlook.cz>
2021-04-20 03:53:46 +0200

    debug

--- player.py
+++ player.py
@@ -93,7 +93,7 @@ class BluePlayer(dbus.service.Object):
         #self.mainloop.run()
 
     def getInfo(self):
-        self.findPlayer()
+        self.start()
         return self.showTrack()
 
     def findPlayer(self):
