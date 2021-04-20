24f229262eb8f9e1f12eeffefd8f6c3fc6e5148f
KubaBoi <jakubanderle@outlook.cz>
2021-04-20 02:08:39 +0200

    debug

--- main.py
+++ main.py
@@ -137,16 +137,16 @@ class SampleApp(tk.Tk):
         data = r.json()
 
     def getDevices(self):
-    output = check_output(['hcitool', 'con']).decode("utf-8") 
-    lines = output.split(">")
+        output = check_output(['hcitool', 'con']).decode("utf-8") 
+        lines = output.split(">")
 
-    devices = []
-    for d in range(1, len(lines)):
-        mac = lines[d].strip().split(" ")[1] #ziska mac adresu
-        name = check_output(["hcitool", "name", mac]).decode("utf-8").rstrip() #ziska jmeno zarizeni
-        devices.append((name, mac))
+        devices = []
+        for d in range(1, len(lines)):
+            mac = lines[d].strip().split(" ")[1] #ziska mac adresu
+            name = check_output(["hcitool", "name", mac]).decode("utf-8").rstrip() #ziska jmeno zarizeni
+            devices.append((name, mac))
 
-    return devices
+        return devices
 
 if __name__== "__main__":
     try:
